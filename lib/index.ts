01c23acdc7b51cf4fcc9769cd3d0259b824e6670
Jack <jackm@example.com>
2026-08-30 15:31:53 +0100

    cleanup: remove dead code (lib/index.ts, scripts/, docs/)
    - lib/index.ts: unused barrel export (components import directly from modules)
    - scripts/update-blog.js: obsolete (lib/blog.ts no longer exists; blog uses /api/blog endpoint)
    - docs/: unused documentation

--- lib/index.ts
+++
@@ -1,6 +0,0 @@
-export { cn } from "./utils";
-export { getWebLogger } from "./logger";
-export { SITE_CONFIG, BLOG_CONFIG, RATE_LIMIT_CONFIG } from "./config";
-export type { BlogPost } from "./types";
-export type { Technology, Company, Project } from "./content";
-export { FEATURED_PROJECTS, TECHNOLOGIES_LEARNED, MASONRY_ITEMS } from "./content";
